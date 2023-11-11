88b200f4ff6c6c3507f7f3614e0530ff49e83dbf
osawa-koki <koki@osawa.dev>
2023-11-11 11:34:29 +0900

    python -m flake8 ./app/ ./tests/

--- app/main.py
+++ app/main.py
@@ -2,7 +2,9 @@ import argparse
 
 from rc4 import decrypt, encrypt
 
-parser = argparse.ArgumentParser(description="Encrypt and decrypt messages using RC4.")
+parser = argparse.ArgumentParser(
+    description="Encrypt and decrypt messages using RC4."
+)
 parser.add_argument("-k", "--key", type=str, help="key")
 parser.add_argument("-m", "--message", type=str, help="message")
 
