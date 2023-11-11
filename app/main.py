7acbe23c542ee7e85019bb2081586f1bf70f5709
osawa-koki <koki@osawa.dev>
2023-11-11 11:32:54 +0900

    メイン実行スクリプトの作成。

--- app/main.py
+++ app/main.py
@@ -1,12 +1,18 @@
-from rc4 import decrypt, encrypt
+import argparse
 
-key = "key"
-message = "message"
+from rc4 import encrypt, decrypt
 
-encrypted = encrypt(message, key)
+parser = argparse.ArgumentParser(
+    description='Encrypt and decrypt messages using RC4.'
+)
+parser.add_argument("-k", "--key", type=str, help="key")
+parser.add_argument("-m", "--message", type=str, help="message")
 
-print(encrypted)
+args = parser.parse_args()
+key = args.key
+message = args.message
 
-decrypted = decrypt(encrypted, key)
-
-print(decrypted)
+encrypted_message = encrypt(message, key)
+print("Encrypted message: ", encrypted_message)
+decrypted_message = decrypt(encrypted_message, key)
+print("Decrypted message: ", decrypted_message)
